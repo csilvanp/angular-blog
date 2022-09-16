f78b86ab08a9b2c4c2c35f86c6147591e5d91651
Cauã Silva <csilvanp@gmail.com>
2022-09-16 16:53:37 -0300

    app updated with home module & component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './pages/home/home.module';
+
 import { AppComponent } from './app.component';
-import { BlogTitleComponent } from './components/blog-title/blog-title.component';
-import { BlogMainCardComponent } from './components/blog-main-card/blog-main-card.component';
-import { BlogSubCardComponent } from './components/blog-sub-card/blog-sub-card.component';
-import { BlogMenuComponent } from './components/blog-menu/blog-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogTitleComponent,
-    BlogMainCardComponent,
-    BlogSubCardComponent,
-    BlogMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
