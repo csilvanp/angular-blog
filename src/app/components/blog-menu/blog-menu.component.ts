cd2768e084780a193c6405bc1fa12b97254fe062
Cauã Silva <csilvanp@gmail.com>
2022-09-16 16:54:20 -0300

    updated menu component & added dynamic data

--- src/app/components/blog-menu/blog-menu.component.ts
+++ src/app/components/blog-menu/blog-menu.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment'
 
 @Component({
   selector: 'app-blog-menu',
   templateUrl: './blog-menu.component.html',
   styleUrls: ['./blog-menu.component.css']
 })
-export class BlogMenuComponent implements OnInit {
+export class BlogMenuComponent {
 
-  constructor() { }
+  items:string[];
 
-  ngOnInit(): void {
+  constructor() { 
+    this.items = environment.menu_items;
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  menu_items: ["Home", "Blog", "Bookmarks", "Elements", "Playlists", "Books"],
 };
 
 /*
